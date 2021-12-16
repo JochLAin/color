dca2c809a3d2cde07d6ee206380379108f6fd082
jocelyn <jocelyn@amtsolutions.fr>
2021-12-16 17:31:13 +0100

    WIP: add indexes

--- lib/index.ts
+++ lib/index.ts
@@ -1,10 +1,12 @@
-import { HEX_REGEX, HSL_REGEX, HWB_REGEX, RGB_REGEX, NAMES, HEX_NAMES } from "./constants";
-import { createHEX, createHSL, createRGB } from "./factories";
 import Color from "./models";
 import { COLOR_INPUT, HSL, RGB } from "./types";
-import { isSCMYK, isCMYK, isHEX, isSHSL, isHSL, isSHWB, isHWB, isINT, isNCOL, isSRGB, isRGB } from "./utils";
+import { createHEX, createHSL, createRGB } from "./factories";
 
-export const paint = (props: COLOR_INPUT, format: string = 'rgb') => {
+export { HEX_REGEX, HSL_REGEX, HWB_REGEX, RGB_REGEX, NAMES, HEX_NAMES } from "./constants";
+export { COLOR_INPUT, HSL, RGB } from "./types";
+export { isSCMYK, isCMYK, isHEX, isSHSL, isHSL, isSHWB, isHWB, isINT, isNCOL, isSRGB, isRGB } from "./utils";
+
+export default (props: COLOR_INPUT, format: string = 'rgb') => {
     return Color.create(props, format);
 };
 
@@ -15,30 +17,3 @@ export const random = (format?: string, props?: COLOR_INPUT): Color => {
         default: return new Color(createHEX());
     }
 };
-
-Object.assign(paint, {
-    HEX_REGEX,
-    HSL_REGEX,
-    HWB_REGEX,
-    RGB_REGEX,
-    NAMES,
-    HEX_NAMES,
-    Color,
-    createHEX,
-    createHSL,
-    createRGB,
-});
-
-Object.assign(paint, {
-    isHEX,
-    isINT,
-    isNCOL,
-    isCMYK: (args: any) => isCMYK(args) || isSCMYK(args),
-    isHSL: (args: any) => isHSL(args) || isSHSL(args),
-    isHWB: (args: any) => isHWB(args) || isSHWB(args),
-    isRGB: (args: any) => isRGB(args) || isSRGB(args),
-});
-
-Object.assign(paint, { random });
-
-export default paint;
